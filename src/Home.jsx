e8b0c992f65a06fbf09edbedf0ea3bdd085c79ce
Renzzi <renzzi.adorador@gmail.com>
2023-10-19 07:34:50 -0400

    Added content and style to homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Button, Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Landing from './assets/Landing.jpg'
 
 
 export default function Home() {
   const userID = localStorage.getItem('id');
+  const username = localStorage.getItem('username');
   const [name, setName] = useState('');
 
   const getData = async () => {
@@ -25,10 +28,15 @@ export default function Home() {
     <div className='custom-body'>
       <div className='custom-container'>
         <h1>Hey there{name? ` ${name}!`: "!"}</h1>
-        <h2>Kape-Kape is a Filipino phrase which is commonly used when people invites each other for a small talk and a coffee, which inspired this app, a digital business card app,
-           which allows users to create a meaningful connection with just a click of QR code. Developed by 16BitHero.</h2>
-        <Stack spacing={2} direction="row">
-         <Button>Hahahaha</Button>
+        <img src={Landing} alt='Landing' className='landing-image' />
+        <h3 className='intro-text'>'Kape-Kape' is a Filipino phrase commonly used when people invite each other for small talk and coffee. 
+          This phrase inspired our app, a digital business card app that allows users to create meaningful connections with just a click of a QR code.</h3>
+          <h3 className='intro-text'>Developed by 16BitHero.</h3>
+        <br />
+        <Stack spacing={2} direction="row" alignItems="center" justifyContent="center" useFlexGap flexWrap="wrap">
+        <Link to={`/profile/${username}`}><Button variant="contained">View Profile</Button></Link>
+        <Link to="/settings"><Button variant="contained">Update Profile</Button></Link>
+        <Link to="/qrcode"><Button variant="contained">Share QR Code</Button></Link>
         </Stack>
       </div>
     </div>
